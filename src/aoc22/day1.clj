d519165b0e40e30e73abf46bca80d23979d87ed5
Thomas Introini <thomas.introini@gmail.com>
2023-06-11 21:53:23 +0200

    :art: refactor common function

--- src/aoc22/day1.clj
+++ src/aoc22/day1.clj
@@ -5,9 +5,12 @@
 
 (def input (read-day-input "day1" :delimiter "\n\n"))
 
+(defn merge-weights [elves] 
+  (map (comp #(apply + %) #(map string-to-int %) split-lines) elves))
+
 (defn part1 []
-  (apply max (map (comp #(apply + %) #(map string-to-int %) split-lines) input)))
+  (apply max (merge-weights input)))
 
 (defn part2 []
-  (apply + (take-last 3 (sort (map (comp #(apply + %) #(map string-to-int %) split-lines) input)))))
+  (apply + (take-last 3 (sort (merge-weights input)))))
 
